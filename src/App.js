0f662e7e2e3db4123a5a6c374eb436d6f756f388
Helder Sérvio <heldersrvio@gmail.com>
2021-06-06 21:59:51 -0300

    Added period

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Travis CI Example
+          Travis CI Example.
         </a>
       </header>
     </div>
